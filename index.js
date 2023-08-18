fc3c8200cf0d9c0e23e9025b0849fe680be79ad7
krishna1500 <krishna16151@gmail.com>
2023-08-18 20:19:06 +0530

    Store Action Reducer

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("Inside index.js")
+const Redux= require('redux')
+const createConfig= Redux.legacy_createStore
+
+const CakeOrdered= 'CAKE_ORDERED'
+
+const cakeFun= ()=>{
+    return {
+        type:CakeOrdered,
+        quantity:1
+    }
+}
+
+const initialState={
+    noOfCakes:10,
+}
+
+const reducer =(state=initialState,action)=>{   
+    switch(action.type){
+        case 'CAKE_ORDERED':
+            return {
+                noOfCakes: state.noOfCakes-1
+            }
+        default:
+            return state
+    }
+}
+
+const store= createConfig(reducer)
+console.log('Initial state',store.getState())
+
+const unsubscribe=store.subscribe(()=>console.log('update state',store.getState()))
+
+store.dispatch(cakeFun())
+store.dispatch(cakeFun())
+store.dispatch(cakeFun())
+
+store.unsubscribe

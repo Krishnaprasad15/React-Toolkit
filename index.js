a3f531090583090e549c95cff6bf9e1deb4f8b15
krishna1500 <krishna16151@gmail.com>
2023-08-20 17:22:19 +0530

    Multiple reducers

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bind = Redux.bindActionCreators
 
 const CakeOrdered= 'CAKE_ORDERED'
 const CakeRestored= 'CAKE_RESTORED'
+const IceCreamOrd= 'ICECREAM_ORDERED'
+const IceCreamRes= 'ICECREAM_RESTORED'
 
 const cakeFun= ()=>{
     return {
@@ -18,11 +20,28 @@ const CakeRestor= (qty=1)=>{
     }
 }
 
-const initialState={
-    noOfCakes:10,
+const IceFun= (qty=1)=>{
+    return{
+        type:IceCreamOrd,
+        payload:qty
+    }
+}
+const IceRest= (qty=1)=>{
+    return {
+        type:IceCreamRes,
+        payload:qty
+
+    }
 }
 
-const reducer =(state=initialState,action)=>{   
+const initialCakeState={
+    noOfCakes:10,
+}
+const initialIceState={
+    noOfIceCreams:20,
+}
+
+const reducerCake =(state=initialCakeState,action)=>{   
     switch(action.type){
         case 'CAKE_ORDERED':
             return {
@@ -38,20 +57,45 @@ const reducer =(state=initialState,action)=>{
             return state
     }
 }
+const reducerIce = (state=initialIceState,action)=>{
+    switch(action.type){
+        case "ICECREAM_ORDERED":
+            return {
+                ...state,
+                noOfIceCreams: state.noOfIceCreams -1
+            }
+        case "ICECREAM_RESTORED":
+            return{
+                ...state,
+                noOfIceCreams: state.noOfIceCreams + action.payload
+            }
+        default:
+            return state
+    }
+}
 
-const store= createConfig(reducer)
-console.log('Initial state',store.getState())
+const storeCake= createConfig(reducerCake)
+const storeIce= createConfig(reducerIce)
+console.log('Initial state',storeCake.getState())
+console.log('Initial state',storeIce.getState())
 
-const unsubscribe=store.subscribe(()=>console.log('update state',store.getState()))
+const unsubscribe=storeCake.subscribe(()=>console.log('update state',storeCake.getState()))
+const unsubscribeI=storeIce.subscribe(()=>console.log('update state',storeIce.getState()))
 
 // store.dispatch(cakeFun())
 // store.dispatch(cakeFun())
 // store.dispatch(cakeFun())
 // store.dispatch(CakeRestor(3))
-const actions = bind({cakeFun,CakeRestor},store.dispatch)
-actions.cakeFun()
-actions.cakeFun()
-actions.cakeFun()
-actions.CakeRestor(3)
+const actionsCake = bind({cakeFun,CakeRestor},storeCake.dispatch)
+const actionsIce  = bind({IceFun,IceRest},storeIce.dispatch)
+actionsCake.cakeFun()
+actionsCake.cakeFun()
+actionsCake.cakeFun()
+actionsCake.CakeRestor(3)
+actionsIce.IceFun()
+actionsIce.IceFun()
+actionsIce.IceFun()
+actionsIce.IceRest(3)
 
 unsubscribe()
+unsubscribeI()
